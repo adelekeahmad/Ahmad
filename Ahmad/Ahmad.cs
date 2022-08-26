e0cbd0d8994f2df1d8840349372c8619b4a6353b
Ahmad Bakare <65125857+adelekeahmad@users.noreply.github.com>
2022-08-25 20:53:14 -0700

    Add project files.

---
+++ Ahmad/Ahmad.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahmad
+{
+    internal class Ahmad
+    {
+        public string name;
+        public string complexion;
+        public string nationality;
+        public int age;
+        public double height;
+        public int weight;
+        public string religion;
+        public bool handsome;
+        public string ethnicity;
+
+    }
+}
---
+++ Ahmad/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahmad;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Ahmad ahmad = new Ahmad();
+        ahmad.name = "Paapy Jay";
+        ahmad.age = 32;
+        ahmad.complexion = "dark";
+        ahmad.nationality = "Nigerian";
+        ahmad.height = 65;
+        ahmad.weight = 100;
+        ahmad.religion = "islam";
+        ahmad.ethnicity = "yoruba";
+        ahmad.handsome = true;
+        Console.WriteLine(ahmad.name + " is a " + ahmad.complexion + " guy " + "whose age is " + ahmad.age + " and " + "height is " + ahmad.height + ".");
+        Console.ReadLine();
+    }
+    
+}
